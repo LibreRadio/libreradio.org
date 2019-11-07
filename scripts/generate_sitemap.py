7048f8426f501fbcefd59f6f5a9ba507acf9759a
Autom <40275136+AutomCoding@users.noreply.github.com>
2019-11-07 19:29:25 +0100

    Replace tabs with spaces

--- scripts/generate_sitemap.py
+++ scripts/generate_sitemap.py
@@ -1,7 +1,8 @@
 import os
 
 
-directory_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../public/"))
+directory_path = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "../public/"))
 root_lengt = len(directory_path)
 
 files = []
@@ -10,24 +11,25 @@ for directory_name, subdirectory_list, file_list in os.walk(directory_path):
         files.append(os.path.join(directory_name, file_name))
 
 exclude = [
-	"/assets/error_docs/403.html",
-	"/assets/error_docs/404.html",
-	"/documentation/api/response.html"
+    "/assets/error_docs/403.html",
+    "/assets/error_docs/404.html",
+    "/documentation/api/response.html"
 ]
 exclude_list = []
 for file in exclude:
-	exclude_list.append(os.path.abspath(os.path.join(directory_path, file[1:])))
+    exclude_list.append(os.path.abspath(
+        os.path.join(directory_path, file[1:])))
 
 sitemap = ""
 url_root = "https://libreradio.org/"
 for file in files:
-	if file.endswith((".html", ".php", "pdf")):
-		if file in exclude_list:
-			continue
-		elif file.endswith("index.html"):
-			sitemap += url_root + file[root_lengt:-10] + "\n"
-		else:
-			sitemap += url_root + file[root_lengt:] + "\n"
+    if file.endswith((".html", ".php", "pdf")):
+        if file in exclude_list:
+            continue
+        elif file.endswith("index.html"):
+            sitemap += url_root + file[root_lengt:-10] + "\n"
+        else:
+            sitemap += url_root + file[root_lengt:] + "\n"
 
 file = open(os.path.join(directory_path, "sitemap.txt"), "w")
 file.write(sitemap.replace("\\", "/"))
