62502c93bae9b887b5a7192dd2b748abc9181649
Autom <40275136+AutomCoding@users.noreply.github.com>
2019-11-07 19:55:07 +0100

    Simplify script

--- scripts/generate_sitemap.py
+++ scripts/generate_sitemap.py
@@ -1,6 +1,12 @@
 import os
 
 
+exclude_list = [
+    "assets/error_docs/403.html",
+    "assets/error_docs/404.html",
+    "documentation/api/response.html"
+]
+
 directory_path = os.path.abspath(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), "../public/"))
 root_lengt = len(directory_path)
@@ -10,27 +16,17 @@ for directory_name, subdirectory_list, file_list in os.walk(directory_path):
     for file_name in file_list:
         files.append(os.path.join(directory_name, file_name))
 
-exclude = [
-    "/assets/error_docs/403.html",
-    "/assets/error_docs/404.html",
-    "/documentation/api/response.html"
-]
-exclude_list = []
-for file in exclude:
-    exclude_list.append(os.path.abspath(
-        os.path.join(directory_path, file[1:])))
-
-sitemap = ""
+pages = []
 url_root = "https://libreradio.org/"
 for file in files:
     if file.endswith((".html", ".php", "pdf")):
-        if file in exclude_list:
-            continue
-        elif file.endswith("index.html"):
-            sitemap += url_root + file[root_lengt:-10] + "\n"
+        if file.endswith("index.html"):
+            pages.append(file[root_lengt:-10].replace("\\", "/"))
         else:
-            sitemap += url_root + file[root_lengt:] + "\n"
+            pages.append(file[root_lengt:].replace("\\", "/"))
 
 file = open(os.path.join(directory_path, "sitemap.txt"), "w")
-file.write(sitemap.replace("\\", "/"))
+for page in pages:
+    if page not in exclude_list:
+        file.write(url_root + page + "\n")
 file.close()
